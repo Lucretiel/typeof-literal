a52598c5fb7ae21005cab39cdecd783a759561d0
Nathan West <Lucretiel@gmail.com>
2023-09-13 19:03:24 -0600

    Re-implement to avoid the intermediary type, just quote directly

--- src/lib.rs
+++ src/lib.rs
@@ -2,28 +2,11 @@ use std::fmt::{self, Display, Formatter};
 
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use quote::quote;
+use quote::{format_ident, quote};
 use syn::{
-    parse_macro_input, spanned::Spanned, Expr, ExprBlock, ExprGroup, ExprParen, ExprUnsafe, Ident,
-    Lit, Stmt, Type,
+    parse_macro_input, spanned::Spanned, Expr, ExprBlock, ExprCast, ExprGroup, ExprParen,
+    ExprUnsafe, Lit, Stmt,
 };
-
-enum Typename<'a> {
-    // Int, string, bool, etc
-    Ident(syn::Ident),
-    Never,
-    Tuple(Vec<Self>),
-    Array(Box<Self>, ArrayLength<'a>),
-
-    // A real type we got from the expression, perhaps from an `as` cast
-    Type(&'a Type),
-
-    // A reference to some other type. Always static.
-    Reference(Box<Self>),
-
-    // A slice of something
-    Slice(Box<Self>),
-}
 
 struct Error {
     location: Span,
@@ -51,151 +34,101 @@ impl Display for ErrorKind {
     }
 }
 
-#[derive(Clone, Copy)]
-enum ArrayLength<'a> {
-    Raw(usize),
-    Expr(&'a Expr),
+fn default_str<'a>(input: &'a str, default: &'a str) -> &'a str {
+    match input {
+        "" => default,
+        input => input,
+    }
 }
 
-impl ArrayLength<'_> {
-    fn into_tokens(self) -> proc_macro2::TokenStream {
-        match self {
-            ArrayLength::Raw(len) => quote! {#len},
-            ArrayLength::Expr(expr) => quote! {#expr},
+fn typename(expr: &Expr) -> Result<proc_macro2::TokenStream, Error> {
+    let span = expr.span();
+    let error = move |kind| Error {
+        location: span,
+        kind,
+    };
+
+    match expr {
+        // The whole point of this crate:
+        Expr::Lit(literal) => Ok(match literal.lit {
+            Lit::Str(_) => quote! {&'static str},
+            Lit::ByteStr(_) => quote! {&'static [u8]},
+            Lit::Byte(_) => quote! {u8},
+            Lit::Char(_) => quote! {char},
+            Lit::Int(ref i) => {
+                let ty = format_ident!("{}", default_str(i.suffix(), "i32"));
+                quote! {#ty}
+            }
+            Lit::Float(ref f) => {
+                let ty = format_ident!("{}", default_str(f.suffix(), "f64"));
+                quote! {#ty}
+            }
+            Lit::Bool(_) => quote! {bool},
+            _ => return Err(error(ErrorKind::UnrecognizedLiteral)),
+        }),
+
+        // Fun extras:
+        // Composite types: tuple, arrays, reference
+        Expr::Array(array) => {
+            let inner_expr = array.elems.first().ok_or(error(ErrorKind::EmptyArray))?;
+
+            let inner = typename(inner_expr)?;
+            let len = array.elems.len();
+
+            Ok(quote! {[#inner; #len]})
         }
-    }
-}
 
-impl<'a> Typename<'a> {
-    pub fn unit() -> Typename<'static> {
-        Typename::Tuple(Vec::new())
-    }
+        Expr::Repeat(array) => {
+            let inner = typename(&*array.expr)?;
+            let len = &array.len;
+            Ok(quote! {[#inner; #len]})
+        }
 
-    pub fn primitive(name: &str) -> Typename<'static> {
-        Typename::Ident(Ident::new(name, Span::call_site()))
-    }
+        Expr::Reference(reference) => {
+            let inner = typename(&*reference.expr)?;
+            Ok(quote! {&'static #inner})
+        }
 
-    pub fn str() -> Typename<'static> {
-        Typename::Reference(Box::new(Self::primitive("str")))
-    }
-
-    pub fn bytes() -> Typename<'static> {
-        Typename::Reference(Box::new(Typename::Slice(Box::new(Self::primitive("u8")))))
-    }
-
-    // TODO: better errors
-    pub fn from_expr(expr: &'a Expr) -> Result<Self, Error> {
-        let span = expr.span();
-        let error = move |kind| Error {
-            location: span,
-            kind,
-        };
-
-        match expr {
-            // The whole point of this crate:
-            Expr::Lit(literal) => Ok(match literal.lit {
-                Lit::Str(_) => Self::str(),
-                Lit::ByteStr(_) => Self::bytes(),
-                Lit::Byte(_) => Self::primitive("u8"),
-                Lit::Char(_) => Self::primitive("char"),
-                Lit::Int(ref i) => Self::primitive(match i.suffix() {
-                    "" => "i32",
-                    suffix => suffix,
-                }),
-                Lit::Float(ref f) => Self::primitive(match f.suffix() {
-                    "" => "f64",
-                    suffix => suffix,
-                }),
-                Lit::Bool(_) => Self::primitive("bool"),
-                _ => return Err(error(ErrorKind::UnrecognizedLiteral)),
-            }),
-
-            // Fun extras:
-            // Composite types: tuple, arrays, reference
-            Expr::Array(array) => {
-                // TODO: theoretically we could scan until we find a literal
-                // and use that, to allow expressions like `[a, b, "hello"]`
-                let inner_expr = array.elems.first().ok_or(error(ErrorKind::EmptyArray))?;
-                let inner = Typename::from_expr(inner_expr)?;
-                let len = array.elems.len();
-                Ok(Typename::Array(Box::new(inner), ArrayLength::Raw(len)))
-            }
-
-            Expr::Repeat(array) => {
-                let inner = Typename::from_expr(&*array.expr)?;
-                let len = ArrayLength::Expr(&*array.len);
-                Ok(Typename::Array(Box::new(inner), len))
-            }
-
-            Expr::Reference(reference) => Self::from_expr(&*reference.expr)
-                .map(Box::new)
-                .map(Self::Reference),
-
-            Expr::Tuple(t) => t
+        Expr::Tuple(t) => {
+            let types = t
                 .elems
                 .iter()
-                .map(Self::from_expr)
-                .collect::<Result<_, _>>()
-                .map(Self::Tuple),
+                .map(typename)
+                .collect::<Result<Vec<_>, _>>()?;
 
-            // Block-like things; just look to see if there's a literal in
-            // the tail position
-            Expr::Unsafe(ExprUnsafe { block, .. }) | Expr::Block(ExprBlock { block, .. }) => {
-                // Find the expression without a semicolon. Don't bother trying
-                // to find `return` expressions.
-                let tail = block.stmts.iter().find_map(|statement| match statement {
-                    Stmt::Expr(expr, None) => Some(expr),
-                    _ => None,
-                });
-
-                match tail {
-                    Some(expr) => Self::from_expr(expr),
-                    None => Ok(Self::unit()),
-                }
-            }
-
-            Expr::Group(ExprGroup { expr, .. }) | Expr::Paren(ExprParen { expr, .. }) => {
-                Self::from_expr(&**expr)
-            }
-
-            // Control flow stuff has type `!`
-            Expr::Return(_) | Expr::Break(_) | Expr::Continue(_) => Ok(Self::Never),
-
-            // Casts are great cause they give us the literal type. Not really
-            // clear why you're using this crate in this case, but we're not
-            // here to judge.
-            Expr::Cast(cast) => Ok(Self::Type(&*cast.ty)),
-
-            // Assignments are valid expressions of unit type
-            Expr::Assign(_) => Ok(Self::unit()),
-
-            _ => Err(error(ErrorKind::UnsupportedExpression)),
+            Ok(quote! {(#(#types,)*)})
         }
-    }
 
-    pub fn into_tokens(self) -> proc_macro2::TokenStream {
-        match self {
-            Typename::Ident(ident) => quote! {#ident},
-            Typename::Never => quote! {!},
-            Typename::Tuple(names) => {
-                let names = names.into_iter().map(|name| name.into_tokens());
-                quote! {( #(#names,)* )}
-            }
-            Typename::Array(name, len) => {
-                let name = name.into_tokens();
-                let len = len.into_tokens();
-                quote! { [#name; #len] }
-            }
-            Typename::Type(ty) => quote! {#ty},
-            Typename::Reference(inner) => {
-                let inner = inner.into_tokens();
-                quote! { &'static #inner }
-            }
-            Typename::Slice(inner) => {
-                let inner = inner.into_tokens();
-                quote! { [#inner] }
+        // Block-like things; just look to see if there's a literal in
+        // the tail position
+        Expr::Unsafe(ExprUnsafe { block, .. }) | Expr::Block(ExprBlock { block, .. }) => {
+            // Find the expression without a semicolon. Don't bother trying
+            // to find `return` expressions.
+            let tail = block.stmts.iter().find_map(|statement| match statement {
+                Stmt::Expr(expr, None) => Some(expr),
+                _ => None,
+            });
+
+            match tail {
+                Some(expr) => typename(expr),
+                None => Ok(quote! {()}),
             }
         }
+
+        Expr::Group(ExprGroup { expr, .. }) | Expr::Paren(ExprParen { expr, .. }) => typename(expr),
+
+        // Control flow stuff has type `!`
+        Expr::Return(_) | Expr::Break(_) | Expr::Continue(_) => Ok(quote! {!}),
+
+        // Casts are great cause they give us the literal type. Not really
+        // clear why you're using this crate in this case, but we're not
+        // here to judge.
+        Expr::Cast(ExprCast { ty, .. }) => Ok(quote! {#ty}),
+
+        // Assignments are valid expressions of unit type
+        Expr::Assign(_) => Ok(quote! {()}),
+
+        _ => Err(error(ErrorKind::UnsupportedExpression)),
     }
 }
 
@@ -203,8 +136,8 @@ impl<'a> Typename<'a> {
 pub fn typeof_literal(expr: TokenStream) -> TokenStream {
     let expr = parse_macro_input!(expr as Expr);
 
-    match Typename::from_expr(&expr) {
-        Ok(name) => name.into_tokens(),
+    match typename(&expr) {
+        Ok(name) => name,
         Err(err) => syn::Error::new(err.location, err.kind).into_compile_error(),
     }
     .into()
